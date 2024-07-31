f7278758e1c72acebbb298ccba865c9511f7d4df
huynd <nguyendanghuy071195@gmail.com>
2024-07-31 17:27:51 +0700

    update status for gltf computer canvas

--- components/computer/index.tsx
+++ components/computer/index.tsx
@@ -2,10 +2,85 @@ import React from 'react';
 import { Suspense, useEffect, useState } from 'react';
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
+import * as dat from 'dat.gui';
 
-import CanvasLoader from '../Loader';
+interface ComputersInterfaceProps {
+  isMobile: boolean;
+}
+const Computers: React.FC<ComputersInterfaceProps> = ({ isMobile }) => {
+  const computers = useGLTF('/gaming_desktop_pc/scene.gltf');
+  return (
+    <mesh>
+      <hemisphereLight
+        intensity={0.15}
+        groundColor={'black'}
+      />
+      <pointLight intensity={1} />
+      <spotLight
+        position={[-20, 50, 10]}
+        angle={0.3}
+        penumbra={1}
+        intensity={1}
+        castShadow
+        shadow-mapSize={1024}
+      />
+      <primitive
+        object={computers.scene}
+        scale={isMobile ? 0.01 : 0.75}
+        position={isMobile ? [-5, -2, -2.2] : [0, -3.25, -1.5]}
+        rotation={[-0.01, -0.2, -0.1]}
+      />
+    </mesh>
+  );
+};
 const ComputerCanvas = () => {
-  return <div>ComputerCanvas</div>;
+  const [isMobile, setIsMobile] = useState(false);
+  const [cameraPosition, setCameraPosition] = useState({ x: 20, y: 3, z: 5 });
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia('(max-width: 500)');
+    setIsMobile(mediaQuery.matches);
+    const handleMediaQueryChange = (e: any) => setIsMobile(e.matches);
+    mediaQuery.addEventListener('change', handleMediaQueryChange);
+    return () => mediaQuery.removeEventListener('change', handleMediaQueryChange);
+  }, []);
+
+  useEffect(() => {
+    const gui = new dat.GUI();
+    const cameraFolder = gui.addFolder('Camera Position');
+    cameraFolder
+      .add(cameraPosition, 'x', -50, 50)
+      .onChange((value) => setCameraPosition({ ...cameraPosition, x: value }));
+    cameraFolder
+      .add(cameraPosition, 'y', -50, 50)
+      .onChange((value) => setCameraPosition({ ...cameraPosition, y: value }));
+    cameraFolder
+      .add(cameraPosition, 'z', -50, 50)
+      .onChange((value) => setCameraPosition({ ...cameraPosition, z: value }));
+    cameraFolder.open();
+
+    return () => {
+      gui.destroy();
+    };
+  }, [cameraPosition]);
+
+  return (
+    <Canvas
+      frameloop="demand"
+      shadows
+      camera={{ position: [20, 3, 5], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true }}>
+      <Suspense fallback={null}>
+        <OrbitControls
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          maxAzimuthAngle={Math.PI / 2}
+        />
+        <Preload all />
+        <Computers isMobile={isMobile} />
+      </Suspense>
+    </Canvas>
+  );
 };
 
 export default ComputerCanvas;
--- components/layout/Hero.tsx
+++ components/layout/Hero.tsx
@@ -1,23 +1,27 @@
 import React from 'react';
 import { motion } from 'framer-motion';
+import ComputerCanvas from '../computer';
 const Hero = () => {
   return (
-    <section className="relative w-full h-screen mx-auto">
-      <div className="absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-grow items-start gap-5">
-        <div className="flex flex-col justify-center items-center mt-5">
-          <div className="w-5 h-5 rounded-full bg-[#915eff]"></div>
-          <div className="w-1 sm:h-80 h-40 bg-violet-gradient" />
+    <>
+      <section className="relative w-full h-screen mx-auto">
+        <div className="absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-grow items-start gap-5">
+          <div className="flex flex-col justify-center items-center mt-5">
+            <div className="w-5 h-5 rounded-full bg-[#915eff]"></div>
+            <div className="w-1 sm:h-80 h-40 bg-violet-gradient" />
+          </div>
+          <div>
+            <h1 className="mt-8 text-[30px]">
+              Hi, I'm <span className="text-[#915eff]">HuyItto</span>
+            </h1>
+            <p className="mt-2 text-white-100 text-[20px]">
+              I'm React developer, user <br className="sm:block hidden" /> interface and web applications
+            </p>
+          </div>
         </div>
-        <div>
-          <h1 className="mt-8 text-[30px]">
-            Hi, I'm <span className="text-[#915eff]">HuyItto</span>
-          </h1>
-          <p className="mt-2 text-white-100 text-[20px]">
-            I'm React developer, user <br className="sm:block hidden" /> interface and web applications
-          </p>
-        </div>
-      </div>
-    </section>
+        <ComputerCanvas />
+      </section>
+    </>
   );
 };
 
