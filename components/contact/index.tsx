b79b6d631bb5547d7d0e9d99d43bf353af3b45b2
huynd <nguyendanghuy071195@gmail.com>
2024-08-22 17:23:54 +0700

    update the form

--- components/contact/index.tsx
+++ components/contact/index.tsx
@@ -3,12 +3,12 @@ import React from 'react';
 
 const Contact = () => {
   return (
-    <div className="w-full  z-10 p-6  bg-[#2d1064] text-white bg-opacity-90 shadow-lg rounded-lg max-w-xl">
+    <div className="w-full  z-10 p-24  bg-[#2d1064] text-white bg-opacity-90 shadow-lg rounded-lg max-w-screen-md">
       <h1 className="text-4xl font-semibold text-center mb-4">Contact Me</h1>
-      <form className="space-y-4">
+      <form className="space-y-12 mt-[36px]">
         <div>
           <label
-            className="block font-medium text-[14px]"
+            className="block font-medium text-[24px]"
             htmlFor="name">
             Name
           </label>
@@ -16,13 +16,13 @@ const Contact = () => {
             type="text"
             id="name"
             name="name"
-            className="mt-1 block w-full p-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500"
+            className="mt-2 block w-full p-4 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500"
             required
           />
         </div>
         <div>
           <label
-            className="block font-medium text-[14px]"
+            className="block font-medium text-[24px]"
             htmlFor="email">
             Email
           </label>
@@ -30,27 +30,27 @@ const Contact = () => {
             type="email"
             id="email"
             name="email"
-            className="mt-1 block w-full p-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500"
+            className="mt-2 block w-full p-4 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500"
             required
           />
         </div>
         <div>
           <label
-            className="block font-medium text-[14px]"
+            className="block font-medium text-[24px]"
             htmlFor="message">
             Message
           </label>
           <textarea
             id="message"
             name="message"
-            rows={4}
-            className="mt-1 block w-full p-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500"
+            rows={10}
+            className="mt-2 block w-full p-4 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500"
             required></textarea>
         </div>
         <div className="text-center">
           <button
             type="submit"
-            className="inline-flex items-center px-4 py-2 bg-[#1e5e4a] border border-transparent rounded-md font-semibold text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+            className="inline-flex items-center px-5 py-2 bg-[#1e5e4a] border border-transparent rounded-md font-semibold text-white text-[24px] hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
             Send Message
           </button>
         </div>
--- components/layout/Hero.tsx
+++ components/layout/Hero.tsx
@@ -4,7 +4,7 @@ import ComputerCanvas from '../computer';
 const Hero = () => {
   return (
     <>
-      <section className="relative w-full h-screen mx-auto">
+      <section className="relative w-full h-screen mx-auto ">
         <div className="absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-grow items-start gap-5">
           <div className="flex flex-col justify-center items-center mt-5">
             <div className="w-5 h-5 rounded-full bg-[#915eff]"></div>
@@ -15,7 +15,10 @@ const Hero = () => {
               Hi, I'm <span className="text-[#915eff]">HuyItto</span>
             </h1>
             <p className="mt-2 text-white-100 text-[20px]">
-              I'm React developer, user <br className="sm:block hidden" /> interface and web applications
+              I'm React developer, user <br className="sm:block hidden" /> interface and web applications I'm also a 3D
+              developer, specializing in creating immersive and interactive experiences using cutting-edge technologies.
+              With my expertise in both React and 3D development, I can bring your ideas to life and deliver stunning
+              visual experiences that engage and captivate users.
             </p>
           </div>
         </div>
